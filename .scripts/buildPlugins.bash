454c32d0438ba5fd07fa404b87b64c02c577bad8
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-25 20:53:58 +0100

    Remove unneeded echo's

--- .scripts/buildPlugins.bash
+++ .scripts/buildPlugins.bash
@@ -1,6 +1,4 @@
 for d in ./.obsidian/plugins/*/ ; do (
-    echo "Building $d"
-
     cd $d
     if [ $d != "./.obsidian/plugins/excalidraw/" ]; then
         npm run dev
@@ -9,7 +7,6 @@ for d in ./.obsidian/plugins/*/ ; do (
     if [ $d = "./.obsidian/plugins/excalidraw/" ]; then
         npm run build
 
-        echo "Copying excalidraw files"
         cp -r ./dist/* .
     fi
 ); done
