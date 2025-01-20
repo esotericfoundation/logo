79aad71694a6a7d1e9efecc2972f35e209a43357
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-20 19:51:23 +0000

    Get another point of the small triangles

--- .scripts/create.py
+++ .scripts/create.py
@@ -65,6 +65,21 @@ right_triangle_top_left = (intersection_right + sqrt(((object_from_triangle_dist
 
 left_triangle_top_right = (intersection_left - sqrt(((object_from_triangle_distance) ** 2) / 2))
 
+# Line 1: (x - w/2) + w/2 - sqrt(2) * d - s_d
+# Line 2 (Triangle's right side): -h/(s/2) * (x - w/2) + w/2 + h/2
+# -h/(s/2) * (x - w/2) + w/2 + h/2 = x - sqrt(2) * d - s_d
+# => s_d + sqrt(2) * d - h/(s/2) * x + hw/s + w/2 + h/2 = x
+# => s_d + sqrt(2) * d + hw/s + w/2 + h/2 = x(1 + h/(s/2))
+# => (s_d + sqrt(2) * d + hw/s + w/2 + h/2)/(1 + h/(s/2)) = x
+
+intersection_right_bottom = (small_square_diagonal + sqrt(2) * object_from_triangle_distance + triangle_height * square_width / triangle_side_length + square_width / 2 + triangle_height / 2)/(1 + triangle_height/(triangle_side_length/2))
+
+intersection_left_bottom = square_width / 2 - (intersection_right_bottom - square_width / 2)
+
+right_triangle_bottom = (intersection_right_bottom + sqrt(((object_from_triangle_distance) ** 2) / 2))
+
+left_triangle_bottom = (intersection_left_bottom - sqrt(((object_from_triangle_distance) ** 2) / 2))
+
 # SVG CREATION:
 
 svg_string = f"<svg width=\"{square_width}\" height=\"{square_width}\" xmlns=\"http://www.w3.org/2000/svg\">\n"
