a1c1ae5eb78282dc6770122268bd238608316a92
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-20 19:33:40 +0000

    Calculate left an right intersections

--- .scripts/create.py
+++ .scripts/create.py
@@ -32,6 +32,21 @@ triangle_right = (triangle_bottom[0] + triangle_side_length / 2, triangle_bottom
 
 triangle_left = (triangle_bottom[0] - triangle_side_length / 2, triangle_bottom[1])
 
+# Line 1: (x - w/2) + w/2
+# Line 2 (Triangle's right side): -h/(s/2) * (x - w/2) + w/2 + h/2
+# -h/(s/2) * (x - w/2) + w/2 + h/2 = x
+# => -h/(s/2) * x + hw/s + w/2 + h/2 = x
+# => hw/s + w/2 + h/2 = x(1 + h/(s/2))
+# => (hw/s + w/2 + h/2)/(1 + h/(s/2)) = x
+
+intersection_right = (triangle_height * square_width / triangle_side_length + square_width / 2 + triangle_height / 2)/(1 + triangle_height/(triangle_side_length/2))
+
+print("Right intersection: " + str(intersection_right))
+
+intersection_left = square_width / 2 - (intersection_right - square_width / 2)
+
+print("Left intersection: " + str(intersection_left))
+
 svg_string = f"<svg width=\"{square_width}\" height=\"{square_width}\" xmlns=\"http://www.w3.org/2000/svg\">\n"
 
 svg_string += f"  <polygon points=\"{point_to_svg_point(triangle_left)} {point_to_svg_point(center)} {point_to_svg_point(triangle_right)} {point_to_svg_point(triangle_top)}\" fill=\"white\" />\n"
