1a8a8615d8ba01e49114ed59894d1533bd181403
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-20 20:00:10 +0000

    Add very wrong calculation

--- .scripts/create.py
+++ .scripts/create.py
@@ -46,6 +46,9 @@ small_square_bottom = (small_square_center[0], small_square_center[1] - small_sq
 small_square_right = (small_square_center[0] + small_square_diagonal / 2, small_square_center[1])
 small_square_left = (small_square_center[0] - small_square_diagonal / 2, small_square_center[1])
 
+def triangle_right_line(x: float):
+    return -triangle_height / (triangle_side_length / 2) * (x - square_width / 2) + square_width / 2 + triangle_height / 2
+
 # Line 1: (x - w/2) + w/2 - sqrt(2) * d
 # Line 2 (Triangle's right side): -h/(s/2) * (x - w/2) + w/2 + h/2
 # -h/(s/2) * (x - w/2) + w/2 + h/2 = x - sqrt(2) * d
@@ -61,9 +64,9 @@ intersection_left = square_width / 2 - (intersection_right - square_width / 2)
 
 print("Left intersection: " + str(intersection_left))
 
-right_triangle_top_left = (intersection_right + sqrt(((object_from_triangle_distance) ** 2) / 2))
+right_triangle_top_left = (intersection_right + sqrt(((object_from_triangle_distance) ** 2) / 2), triangle_right_line(intersection_right + sqrt(((object_from_triangle_distance) ** 2) / 2)))
 
-left_triangle_top_right = (intersection_left - sqrt(((object_from_triangle_distance) ** 2) / 2))
+left_triangle_top_right = (intersection_left - sqrt(((object_from_triangle_distance) ** 2) / 2), right_triangle_top_left[0])
 
 # Line 1: (x - w/2) + w/2 - sqrt(2) * d - s_d
 # Line 2 (Triangle's right side): -h/(s/2) * (x - w/2) + w/2 + h/2
@@ -76,9 +79,18 @@ intersection_right_bottom = (small_square_diagonal + sqrt(2) * object_from_trian
 
 intersection_left_bottom = square_width / 2 - (intersection_right_bottom - square_width / 2)
 
-right_triangle_bottom = (intersection_right_bottom + sqrt(((object_from_triangle_distance) ** 2) / 2))
+right_triangle_bottom = (intersection_right_bottom + sqrt(((object_from_triangle_distance) ** 2) / 2), triangle_right_line(intersection_right_bottom + sqrt(((object_from_triangle_distance) ** 2) / 2)))
 
-left_triangle_bottom = (intersection_left_bottom - sqrt(((object_from_triangle_distance) ** 2) / 2))
+left_triangle_bottom = (intersection_left_bottom - sqrt(((object_from_triangle_distance) ** 2) / 2), right_triangle_bottom[1])
+
+# Line 1: (x - w/2) + w/2 - sqrt(2) * d - s_d
+# Line 2 (Triangle's right side): i_2
+# i_2 = x - sqrt(2) * d - s_d
+# => s_d + sqrt(2) * d + i_2 = x
+# => s_d + sqrt(2) * d + i_2 = x(1 + h/(s/2))
+# => (s_d + sqrt(2) * d + i_2)/(1 + h/(s/2)) = x
+
+right_triangle_right = ((small_square_diagonal + sqrt(2) * object_from_triangle_distance + right_triangle_top_left[1])/(1 + triangle_height/(triangle_side_length / 2)), right_triangle_top_left[1])
 
 # SVG CREATION:
 
@@ -86,6 +98,7 @@ svg_string = f"<svg width=\"{square_width}\" height=\"{square_width}\" xmlns=\"h
 
 svg_string += f"  <polygon points=\"{point_to_svg_point(triangle_left)} {point_to_svg_point(center)} {point_to_svg_point(triangle_right)} {point_to_svg_point(triangle_top)}\" fill=\"white\" />\n"
 svg_string += f"  <polygon points=\"{point_to_svg_point(small_square_top)} {point_to_svg_point(small_square_right)} {point_to_svg_point(small_square_bottom)} {point_to_svg_point(small_square_left)}\" fill=\"white\" />\n"
+svg_string += f"  <polygon points=\"{point_to_svg_point(right_triangle_bottom)} {point_to_svg_point(right_triangle_right)} {point_to_svg_point(right_triangle_top_left)}\" fill=\"white\" />\n"
 
 svg_string += "</svg>\n"
 
