9a8bd24ba3a9779ea3c922573067ffb21fb47612
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-20 22:16:51 +0000

    Export into all resolutions

--- scripts/export.sh
+++ scripts/export.sh
@@ -3,4 +3,12 @@
 npx tsx ./scripts/create.ts
 
 mkdir logo/png
-npx svgexport "./logo/svg/logo.svg" "logo/png/512x512.png"
+npx svgexport "./logo/svg/logo.svg" "logo/png/16x16.png" 16:
+npx svgexport "./logo/svg/logo.svg" "logo/png/32x32.png" 32:
+npx svgexport "./logo/svg/logo.svg" "logo/png/64x64.png" 64:
+npx svgexport "./logo/svg/logo.svg" "logo/png/128x128.png" 128:
+npx svgexport "./logo/svg/logo.svg" "logo/png/256x256.png" 256:
+npx svgexport "./logo/svg/logo.svg" "logo/png/512x512.png" 512:
+npx svgexport "./logo/svg/logo.svg" "logo/png/1024x1024.png" 1024:
+npx svgexport "./logo/svg/logo.svg" "logo/png/2048x2048.png" 2048:
+npx svgexport "./logo/svg/logo.svg" "logo/png/4096x4096.png" 4096:
