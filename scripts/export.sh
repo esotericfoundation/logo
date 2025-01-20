5fe6b1a75ffc3cd0f677e48afdb77d8602173c39
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-20 21:47:30 +0000

    Export PNG file

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Exports the logo to a PNG file.
-npx svgexport "Esoteric Foundation Logo.svg" "Esoteric Foundation Logo.png"
+npx tsx ./scripts/createSvg.ts
+
+npx svgexport "Esoteric Foundation Logo.svg" "logo/png/512x512.png"
