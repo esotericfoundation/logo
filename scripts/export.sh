f611da21301c40659521ccd5c86c01f34563c6f2
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-22 13:49:53 +0000

    Organise cropped pictures into a folder

--- scripts/export.sh
+++ scripts/export.sh
@@ -5,12 +5,14 @@ npx tsx ./scripts/create.ts
 inkscape ./logo/svg/logo.svg --export-area-drawing --export-plain-svg --export-filename=logo/svg/cropped-logo.svg
 
 mkdir logo/png
-npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/0.03125x.png" 0.03125x
-npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/0.0625x.png" 0.0625x
-npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/0.125x.png" 0.125x
-npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/0.25x.png" 0.25x
-npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/0.5x.png" 0.5x
-npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/1x.png"
-npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/2x.png" 2x
-npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/4x.png" 4x
-npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/8x.png" 8x
+mkdir logo/png/cropped
+
+npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/cropped/0.03125x.png" 0.03125x
+npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/cropped/0.0625x.png" 0.0625x
+npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/cropped/0.125x.png" 0.125x
+npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/cropped/0.25x.png" 0.25x
+npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/cropped/0.5x.png" 0.5x
+npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/cropped/1x.png"
+npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/cropped/2x.png" 2x
+npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/cropped/4x.png" 4x
+npx svgexport "./logo/svg/cropped-logo.svg" "logo/png/cropped/8x.png" 8x
