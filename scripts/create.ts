eb7daf7e37d1cde1d4483f6f8ecca6ca9c725639
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-22 14:09:00 +0000

    Remove log

--- scripts/create.ts
+++ scripts/create.ts
@@ -4,7 +4,5 @@ import { logo } from "../functions/logo";
 const svg = logo();
 const squareSvg = logo(true);
 
-console.log(squareSvg)
-
 writeFileSync("./logo/svg/logo.svg", svg)
 writeFileSync("./logo/svg/square-logo.svg", squareSvg);
