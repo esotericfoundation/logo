2ca1dc9c8adc093f7dce330944e4102a3fa77147
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-13 15:19:41 +0000

    Use sharp to export SVG to PNG

--- functions/svgToPng.ts
+++ functions/svgToPng.ts
@@ -1,24 +1,8 @@
-import { createCanvas, loadImage } from 'canvas';
-import { writeFileSync } from 'fs';
+import sharp from "sharp";
 
-export async function convertSvgToPng(svgContent: string, outputFilePath: string, width: number, height: number): Promise<void> {
-    // Create a canvas with specified dimensions
-    const canvas = createCanvas(width, height);
-    const context = canvas.getContext('2d');
-
-    console.log("Loading image...");
-
-    // Load the SVG data as an image
-    const image = await loadImage(`data:image/svg+xml;base64, ${Buffer.from(svgContent).toString('base64')}`);
-
-    console.log("Drawing image...")
-
-    // Draw the SVG onto the canvas
-    context.drawImage(image, 0, 0, width, height);
-
-    // Save the canvas as a PNG file
-    const buffer = canvas.toBuffer('image/png');
-    writeFileSync(outputFilePath, buffer);
-
-    console.log(`PNG saved to ${outputFilePath}`);
+export async function convertSvgToPng(svgContent: string, outputFilePath: string, width: number, height: number) {
+    await sharp(Buffer.from(svgContent))
+        .resize(width, height)
+        .png()
+        .toFile(outputFilePath);
 }
