0409aac4addbb4f3f4b644bddbb0a00a76331195
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-15 11:00:45 +0000

    Remove previous system

--- functions/svgToPng.ts
+++
@@ -1,8 +0,0 @@
-import sharp from "sharp";
-
-export async function convertSvgToPng(svgContent: string, outputFilePath: string, width: number, height: number) {
-    await sharp(Buffer.from(svgContent))
-        .resize(width, height)
-        .png()
-        .toFile(outputFilePath);
-}
--- types/BackgroundType.ts
+++
@@ -1,3 +0,0 @@
-export const backgroundTypes = ["none", "circle", "square"] as const;
-
-export type BackgroundType = typeof backgroundTypes[number];
