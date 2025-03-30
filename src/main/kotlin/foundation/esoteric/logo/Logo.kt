a61c34b4275171fff85e7d3dcfe14a9caadbca27
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-30 20:19:50 +0100

    Successfully open logo file

--- src/main/kotlin/foundation/esoteric/logo/Logo.kt
+++ src/main/kotlin/foundation/esoteric/logo/Logo.kt
@@ -1,5 +1,23 @@
 package foundation.esoteric.logo
 
-fun main() {
+import org.geogebra.desktop.GeoGebra
+import org.geogebra.desktop.gui.app.GeoGebraFrame
+import java.io.File
 
+fun main() {
+    val mainThread = Thread {
+        GeoGebra.main(emptyArray())
+    }.start()
+
+    var frame: GeoGebraFrame? = null
+
+    while (frame == null) {
+        frame = GeoGebraFrame.getActiveInstance()
+    }
+
+    val logoFile = File("./esoteric-foundation-logo.ggb")
+
+    val desktopApp = frame.application
+
+    desktopApp.loadFile(logoFile, false)
 }
