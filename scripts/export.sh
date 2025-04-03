f7122adf63509ee6a750d1dd80fb855c1b384451
Esoteric Enderman <esotericenderman@gmail.com>
2025-04-03 19:40:10 +0100

    Fail export if timed out

--- scripts/export.sh
+++ scripts/export.sh
@@ -10,7 +10,10 @@ gradle desktop:installDist
 
 popd >/dev/null
 
-timeout 30s java -jar ./geogebra/desktop/build/install/desktop/lib/desktop.jar --showSplash=false --showAlgebraInput=false --showAlgebraInputTop=false --showAlgebraWindow=false --showSpreadsheet=false --showCAS=false --show3D=false --logLevel=TRACE ./esoteric-foundation-logo.ggb --export=./esoteric-foundation-logo.svg || echo "Timed out while exporting with GeoGebra"
+timeout 30s java -jar ./geogebra/desktop/build/install/desktop/lib/desktop.jar --showSplash=false --showAlgebraInput=false --showAlgebraInputTop=false --showAlgebraWindow=false --showSpreadsheet=false --showCAS=false --show3D=false --logLevel=TRACE ./esoteric-foundation-logo.ggb --export=./esoteric-foundation-logo.svg || {
+    echo "Timed out while exporting with GeoGebra"
+    exit 1
+}
 
 inkscape ./esoteric-foundation-logo.svg --export-area-drawing --export-plain-svg -o ./esoteric-foundation-logo.svg
 inkscape ./esoteric-foundation-logo.svg -o ./esoteric-foundation-logo.png
