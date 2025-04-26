1608a5d801f5a852dd986ebfb4b1cc4dfcc9711a
Esoteric Enderman <esotericenderman@gmail.com>
2025-04-26 16:10:59 +0100

    Add comments explaining border radius calculations for GitHub
    It's unfortunate that GitHub isn't open source and this can't be done automatically.

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,10 +17,49 @@ asy logo -f svg -u 'type = "circle"; borderDistance = 3' -o './output/logo-circl
 asy logo -f png -u 'type = "square-centered"' -o './output/logo-square-centered'
 asy logo -f svg -u 'type = "square-centered"' -o './output/logo-square-centered'
 
-# GitHub
-# The border distance must be 5 so that the polygon is properly spaced in places such as organisation projects and organisation member profiles.
-# This is because the logo is displayed in some locations with a slight border radius which could cause the polygon to not actually be the same distance from the top and bottom of the logo if its not properly distanced.
-# This border radius needs to be exceeded by the distance from the polygon to the sides of the logo so that it is actually equally spaced from the top and bottom of the logo 
-# A border distance of 5 just happens to be the smallest whole number where this is achieved in all places that the logo is displayed.
-asy logo -f svg -u 'type = "square-equidistant"; borderRadiusFraction = 0.25' -o './output/logo-square-equidistant'
-asy logo -f png -u 'type = "square-equidistant"; borderRadiusFraction = 0.25' -o './output/logo-square-equidistant'
+# Equidistant square
+# Used for: GitHub
+
+# GitHub border-radius-fraction calculations:
+# 1rem = 16px because the font-size property of the root HTML element is 16px.
+
+# The places that GitHub displays organization logos include, but may or may not be limited to:
+
+# The organization homepage
+# Example: https://github.com/esotericfoundation
+# width: 100px
+# height: 100px
+# border-radius: var(--borderRadius-medium) = 0.375rem = 3/8 * 1rem = 3/8 * 16px = 6px
+# border-radius-fraction: 6px/100px = 6/100 = 3/50
+
+# The organization settings page
+# Example: https://github.com/organizations/esotericfoundation/settings/profile
+# width: 200px
+# height: 200px
+# border-radius: var(--borderRadius-medium, 6px) = 0.375rem = 3/8 * 1rem = 3/8 * 16px = 6px
+# border-radius-fraction: 6px/200px = 6/200 = 3/100
+
+# Organization repositories
+# Example: https://github.com/esotericfoundation/logo
+# width: 24px
+# height: 24px
+# border-radius: var(--borderRadius-medium) = 0.375rem = 3/8 * 1rem = 3/8 * 16px = 6px
+# border-radius-fraction: 6px/24px = 6/24 = 1/4
+
+# Organization member pages
+# Example: https://github.com/esotericenderman
+# width: 32px
+# height: 32px
+# border-radius: var(--borderRadius-medium) = 0.375rem = 3/8 * 1rem = 3/8 * 16px = 6px
+# border-radius-fraction: 6px/32px = 6/32 = 3/16
+
+# GitHub homepage (relevant repositories in the organization)
+# Examples: https://github.com/
+# width: 16px
+# height: 16px
+# border-radius: var(--borderRadius-small) = 0.1875rem = 3/16 * 1rem = 3/16 * 16px = 3px
+# border-radius-fraction: 3px/16px = 3/16
+
+# Greatest border-radius-fraction: 1/4
+asy logo -f svg -u 'type = "square-equidistant"; borderRadiusFraction = 1/4' -o './output/logo-square-equidistant'
+asy logo -f png -u 'type = "square-equidistant"; borderRadiusFraction = 1/4' -o './output/logo-square-equidistant'
