9e6e14ac05b83dad4382eb1777e1afc99c1ebe69
Esoteric Enderman <esotericenderman@gmail.com>
2025-04-27 10:53:19 +0100

    Output to correct folder

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,5 +6,5 @@ mkdir -p 'test'
 asy logo -outformat png -outname './test/logo'
 asy logo -outformat svg -outname './test/logo'
 
-asy logo -outformat png -user 'type = "square-centered"' -outname './output/logo-square-centered'
-asy logo -outformat svg -user 'type = "square-centered"' -outname './output/logo-square-centered'
+asy logo -outformat png -user 'type = "square-centered"' -outname './test/logo-square-centered'
+asy logo -outformat svg -user 'type = "square-centered"' -outname './test/logo-square-centered'
