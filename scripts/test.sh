78af3491482a31a5ff3626158faae0bee1a2d6e1
Esoteric Enderman <esotericenderman@gmail.com>
2025-04-27 11:15:51 +0100

    Add all default types to test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,5 +6,11 @@ mkdir -p 'test'
 asy logo -outformat png -outname './test/logo'
 asy logo -outformat svg -outname './test/logo'
 
+asy logo -outformat png -user 'type = "circle"' -outname './test/logo-circle'
+asy logo -outformat svg -user 'type = "circle"' -outname './test/logo-circle'
+
 asy logo -outformat png -user 'type = "square-centered"' -outname './test/logo-square-centered'
 asy logo -outformat svg -user 'type = "square-centered"' -outname './test/logo-square-centered'
+
+asy logo -outformat svg -user 'type = "square-equidistant"' -outname './test/logo-square-equidistant'
+asy logo -outformat png -user 'type = "square-equidistant"' -outname './test/logo-square-equidistant'
